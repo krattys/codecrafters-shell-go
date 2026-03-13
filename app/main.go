02b898954b65fe1ec66953bfae6c2bafc427d078
krattys <145286813+krattys@users.noreply.github.com>
2026-03-13 20:56:59 +0530

    Handle invalid commands gracefully

--- app/main.go
+++ app/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	cmd := strings.Fields(text)[0]
+	fmt.Printf("%s: command not found\n", cmd)
 }
